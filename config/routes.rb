dedac424c636fa5305f23dfa38ba107fc2a8e2c7
ayano0917 <yuki1306ni@yahoo.co.jp>
2019-10-09 01:09:12 +0900

    クレジットカード画面用コントローラANDルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'users#index'
   resources :users, only: [:index,:show ,:update ,:edit]
   resources :items, only: [:index ,:show ,:new, :create, :edit] do
     resources :comments, only: [:index, :create]
-
-  end
+  resources :credit_cards, only: [:new]
 end
