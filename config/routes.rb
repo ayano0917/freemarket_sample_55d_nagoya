ac8135a1875f4fd652c0a51ca06b8a4146dee4bf
KoseiMuramatsu <mukamura219@gmail.com>
2019-10-11 10:10:30 +0900

    add create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users, only: [:index,:show ,:update ,:edit]
+  resources :users, only: [:index,:show ,:create, :update ,:edit]
   resources :items, only: [:index ,:show ,:new, :create, :edit] do
     resources :comments, only: [:index, :create]
   resources :credit_cards, only: [:new]
