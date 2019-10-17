b1be244638044c10ffb05b2c96e91743a2f97cae
tomoharu1205 <tomo611205434@gamil.com>
2019-10-17 18:56:11 +0900

    クレジットカードビュー

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
       end
   end
   resources :comments
-  resources :credit_cards, only: [:new]
+  resources :credit_cards, only: [:new,:index]
   resources :sign_ups, only: [:new, :create] do
       member do
         get :register
