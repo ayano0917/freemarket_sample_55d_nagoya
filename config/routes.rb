f88caeccaa5fd4939d682f256cf7d31859fab4b1
KoseiMuramatsu <mukamura219@gmail.com>
2019-10-20 11:06:07 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,33 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :users, only: [:new, :show, :update, :edit] do
-      member do
-        get :logout
-        get :identification
-      end
 
-      collection do
-        get :complete
-      end
+  resources :users, only: [:new, :show, :update, :edit] do
+    member do
+      get :logout
+      get :identification
+    end
+    collection do
+      get :complete
+    end
   end
+
   resources :items, only: [:new ,:index, :show] do
-      member do
-        get :confirm
-      end
+    member do
+      get :confirm
+    end
   end
+
   resources :comments
   resources :credit_cards, only: [:new, :index, :show]
+
   resources :sign_ups, only: [:new, :create] do
-      member do
-        get :register
-        get :authentication
-        get :address
-        get :pay
-        get :complete
-      end
+    member do
+      get :register
+      get :authentication
+      get :address
+      get :pay
+      get :complete
+    end
   end
 end
