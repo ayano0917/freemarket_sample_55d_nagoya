4cf9a121bf49386c648d51503b35417070d1cfbf
tomoharu1205 <tomo611205434@gamil.com>
2019-10-21 14:23:55 +0900

    sns_credential.rbのバリデーションを削除。

--- app/models/sns_credential.rb
+++ app/models/sns_credential.rb
@@ -1,6 +1,3 @@
 class SnsCredential < ApplicationRecord
   belongs_to :user
-  validates :uid, presnce: true, uniqueness: true
-  validates :costomer_id, presnce: true
-  validates :user_id, presnce: true, uniqueness: true
 end
