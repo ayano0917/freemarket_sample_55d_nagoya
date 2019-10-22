9d25e7c8da283cd4b1e221470c49d975431be1b6
ayano0917 <yuki1306ni@yahoo.co.jp>
2019-10-22 11:40:10 +0900

    add validation

---
+++ db/migrate/20191020133451_add_column_to_users.rb
@@ -0,0 +1,21 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :nickname,       :string,  null: false
+    add_column :users, :last_name,      :string,  null: false
+    add_column :users, :first_name,     :string,  null: false
+    add_column :users, :last_name_kana, :string,  null: false
+    add_column :users, :first_name_kana,:string,  null: false
+    add_column :users, :birth_year,     :integer, null: false
+    add_column :users, :birth_month,    :integer, null: false
+    add_column :users, :birth_day,      :integer, null: false
+    add_column :users, :phone,          :string,  null: false
+    add_column :users, :image,          :string
+    add_column :users, :profile,        :string
+    add_column :users, :sales_amount,   :integer
+    add_column :users, :points,         :integer
+    add_column :users, :Postal_code,    :string
+    add_column :users, :city,           :string
+    add_column :users, :house_number,   :string
+    add_column :users, :building,       :string
+  end
+end
