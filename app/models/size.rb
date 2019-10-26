458d878cc8c39a3474e76ca06615c6305e32153e
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-10-26 17:46:23 +0900

    size.rb updata3

---
+++ app/models/size.rb
@@ -0,0 +1,8 @@
+class Size < ActiveHash::Base
+  self.data = [
+      {id: 1, name: 'XXXS以下'}, {id: 2, name: 'XXS(SS)'}, {id: 3, name: 'S'},
+      {id: 4, name: 'M'}, {id: 5, name: 'L'}, {id: 6, name: 'XL(LL)'},
+      {id: 7, name: '2XL(3L)'}, {id: 8, name: '3XL(4L)'}, {id: 9, name: '4XL(5K)以上'},
+      {id: 10, name: 'FREE SIZE'}
+    
+end
