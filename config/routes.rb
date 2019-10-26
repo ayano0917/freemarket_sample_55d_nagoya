d469ed0d35ca76465055c48feb598bc4397f82f5
KoseiMuramatsu <mukamura219@gmail.com>
2019-10-26 16:52:33 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,33 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks', sessions: "users/sessions"}
   root 'items#index'
-  resources :users, only: [:new, :show, :update, :edit] do
-      member do
-        get :logout
-        get :identification
-      end
 
-      collection do
-        get :complete
-      end
+  resources :users, only: [:new, :show, :update, :edit] do
+    member do
+      get :identification
+    end
+    collection do
+      get :complete
+      get :logout
+    end
   end
-  resources :items, only: [:new, :index, :show, :create] do
-      member do
-        get :confirm
-      end
+
+  resources :items, only: [:new ,:index, :show] do
+    collection do
+      get :confirm
+    end
   end
+
   resources :comments
   resources :credit_cards, only: [:new, :index, :show]
+
   resources :sign_ups, only: [:new, :create] do
-      member do
-        get :register
-        get :authentication
-        get :address
-        get :pay
-        get :complete
-      end
+    collection do
+      get :register
+      get :authentication
+      get :address
+      get :pay
+      get :complete
+    end
   end
 end
