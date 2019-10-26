e5baa0d4d8d87a1191e964838073ca2d6a28d09b
KoseiMuramatsu <mukamura219@gmail.com>
2019-10-26 16:51:43 +0900

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+
+  def new
+    not_used_auth_production
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
