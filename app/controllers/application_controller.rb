a9daac36235566a674268327bf9afcb7e8bbc66a
KoseiMuramatsu <mukamura219@gmail.com>
2019-10-26 16:51:30 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  def configure_permitted_parameters
+    divise_parameter_sanitizer.permit(:)
 
   def production?
     Rails.env.production?
@@ -13,4 +15,13 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
+
+  def not_used_auth_production
+    if Rails.env.production?
+      @facebook_path = "#"
+      @google_path  = "#"
+    else
+      @facebook_path = user_facebook_omniauth_authorize_path
+      @google_path = user_google_oauth2_omniauth_authorize_path
+    end
 end
