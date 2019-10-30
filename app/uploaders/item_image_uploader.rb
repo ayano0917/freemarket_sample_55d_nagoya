757f0bfe2d7109e76423abc20ad494978d420fca
ayano0917 <yuki1306ni@yahoo.co.jp>
2019-10-30 22:08:43 +0900

    delete image-size

--- app/uploaders/item_image_uploader.rb
+++ app/uploaders/item_image_uploader.rb
@@ -2,7 +2,6 @@ class ItemImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
