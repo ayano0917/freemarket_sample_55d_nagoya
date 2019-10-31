d54a87ffe0ecab946837dfa12c7bcf1b776ffddc
KoseiMuramatsu <mukamura219@gmail.com>
2019-10-31 13:06:59 +0900

    payjpの導入により登録画面フロント・サーバー編集

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -2,29 +2,23 @@ class CreditCardsController < ApplicationController
   require "payjp"
   before_action :set_card
 
-  # このindexアクションは仮で設置している
-  def index
-  end
-
-  def new
+  def new #sign-upのクレジットカード登録の画面
     @credit_card = CreditCard.new
     # redirect_to action: "new" if @credit_card.present?
   end
 
   def create
-    Payjp.api_key = 'sk_test_9f89757cd9ede6f6678621dc'
-    
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+
     if params['payjp-token'].blank?
-      # もじトークンが空白であればtrueを返す
-      render action: "new"
+      redirect_to action: "new" #id: current_user.id
     else
       customer = Payjp::Customer.create(
         description: 'test',
         email: current_user.email,
         card: params['payjp-token'],
         metadata: {user_id: current_user.id}
-      )
-
+      ) 
       @credit_card = CreditCard.new(
         user_id: current_user.id,
         customer_id: customer.id,
@@ -33,18 +27,40 @@ class CreditCardsController < ApplicationController
       if @credit_card.save
         redirect_to complete_sign_ups_path
       else
-        render credit_cards_path
+        redirect_to credit_cards_path #id:current_user.id
       end
     end
   end
 
-  def show
-  end
-  
   def destroy
+    card = current_user.credit_cards.first
+    if card.present?
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer.delete
+      card.delete
+    end
+      redirect_to action: "confirmation", id: currrent_user.id
   end
 
+  def show #マイページでクレジットカード情報入力完了後の画面
+    card = current_user.credit_cards.first
+    if card.present?
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    else
+      redirect_to action: "confirmation", id: current_user.id
+    end
+  end
 
+  def edit #マイページクレジットカード情報入力画面
+  end
+
+  def confirmation #マイページのクレジットカードを追加する画面
+    card = current_user.credit_cards
+    redirect_to action: "show" if card.exists?
+  end
+
+  
   private
 
   def set_card
--- db/migrate/20191019083050_create_credit_cards.rb
+++ db/migrate/20191019083050_create_credit_cards.rb
@@ -1,9 +1,9 @@
 class CreateCreditCards < ActiveRecord::Migration[5.0]
   def change
     create_table :credit_cards do |t|
+      t.references :user, null: false, foreign_key: true
       t.string :customer_id, null: false
       t.string :card_id, null: false
-      t.references :user, null: false, foreign_key:true
       t.timestamps
     end
   end
