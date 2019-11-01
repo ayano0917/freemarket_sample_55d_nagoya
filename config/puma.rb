08cd26f594c28f8b8a61f003cd6b4d8e934156f2
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-01 13:20:11 +0900

    facebook認証に伴いhttpsでログインできるように設定(3001のポートを使用)

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-# port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -46,7 +46,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-ssl_bind "0.0.0.0", "3000", {
+ssl_bind "0.0.0.0", "3001", {
   cert: "config/certs/localhost.pem",
   key:  "config/certs/localhost-key.pem"
 }
