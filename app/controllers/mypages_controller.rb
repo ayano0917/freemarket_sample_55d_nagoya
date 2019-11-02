d251798e1ccf22981822aedfdfe7e8809ba00d79
tomoharu1205 <tomo611205434@gamil.com>
2019-11-03 08:26:12 +0900

    updata

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -14,7 +14,7 @@ class MypagesController < ApplicationController
 
   # ここからクレジットカード関連
   def payment  #クレジットカード追加画面
-    card = Card.where(user_id: current_user.id)
+    card = CreditCard.where(user_id: current_user.id)
     redirect_to action: "credit_card_show" if card.exists?
   end
 
@@ -23,13 +23,28 @@ class MypagesController < ApplicationController
   end
 
   def credit_card_show  #クレジットカード登録カード表示
-    card = Card.where(user_id: current_user.id).first
+    card = CreditCard.where(user_id: current_user.id).first
     if card.blank?
       redirect_to action: "create_credit_card" 
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
+      @card_brand = @default_card_information.brand      
+      case @card_brand
+      when "Visa"
+        @card_src = "visa.svg"
+      when "JCB"
+        @card_src = "jcb.svg"
+      when "MasterCard"
+        @card_src = "master-card.svg"
+      when "American Express"
+        @card_src = "american_express.svg"
+      when "Diners Club"
+        @card_src = "dinersclub.svg"
+      when "Discover"
+        @card_src = "discover.svg"
+      end
     end
   end
 
