fab4f308acee33d214533863775979737829d621
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-02 10:56:38 +0900

    マイページコントローラーの作成・ルーティングの変更

---
+++ app/controllers/mypages_controller.rb
@@ -0,0 +1,46 @@
+class MypagesController < ApplicationController
+
+  def show
+  end
+
+  def profile
+  end
+
+  def logout
+  end
+
+  def personal_info
+  end
+
+  # ここからクレジットカード関連
+  def payment
+  end
+
+  def credit_card_reg
+  end
+
+  def credit_card_show
+  end
+
+  def create_credit_card
+  end
+  # ここまで
+
+  def listing_product
+  end
+
+  def shipping_address
+
+  def during_trade
+  end
+
+  def sold
+  end
+
+  def bought_product
+  end
+
+  def past_trade
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,23 @@ Rails.application.routes.draw do
 
   root 'items#index'
 
-
-  resources :users, only: [:new, :create , :show, :update, :edit] do
-    member do
-      get :identification
-    end
-    collection do
-      get :complete
-      get :logout
+  resources :users, only: [:update] do
+    resource :mypage, only: [:show] do
+      collection do
+        get 'profile'
+        get 'logout'
+        get 'personal_info'
+        get 'payment'
+        get 'credit_card_reg'
+        get 'credit_card_show'
+        post 'create_credit_card'
+        get 'listing_product'
+        get 'shipping_address'
+        get 'during_trade'
+        get 'sold'
+        get 'bought_product'
+        get 'past_trade'
+      end
     end
   end
 
@@ -30,7 +39,7 @@ Rails.application.routes.draw do
   resources :user_addresses, only: [:update]
   resources :comments, only: [:new, :create]
   resources :shipping_addresses, only: [:new, :create, :show, :update, :destroy]
-  resources :credit_cards, only: [:index, :new, :create, :show, :destroy]
+  resources :credit_cards, only: [:new, :create, :show, :destroy]
 
   resources :sign_ups, only: [:new, :create] do
     collection do
