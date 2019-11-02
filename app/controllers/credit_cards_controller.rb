d00922f8d4caf995d0c11eddc4f22c528c3816c8
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-02 20:21:46 +0900

    不要な記述・ファイルの削除

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -4,14 +4,13 @@ class CreditCardsController < ApplicationController
 
   def new
     @credit_card = CreditCard.new
-    # redirect_to action: "new" if @credit_card.present?
+    redirect_to action: "new" if @credit_card.present?
   end
 
   def create
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     if params['payjp-token'].blank?
-      binding.pry
-      redirect_to new_credit_card_path #id: current_user.id まだカレントユーザーの実装をしていない為。
+      redirect_to new_credit_card_path
     else
       customer = Payjp::Customer.create(
         card: params['payjp-token'],
@@ -40,29 +39,4 @@ class CreditCardsController < ApplicationController
       redirect_to credit_cards_path
   end
 
-  def show #マイページでクレジットカード情報入力完了後の画面
-    card = current_user.credit_cards.first
-    if card.present?
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-    else
-      redirect_to credit_cards_path#id: current_user.idまだカレントユーザーの実装をしていない為。
-    end
-  end
-
-  def edit #マイページクレジットカード情報入力画面
-  end
-
-  def confirmation #マイページのクレジットカードを追加する画面
-    card = current_user.credit_cards
-    redirect_to credit_cards_path if card.exists?
-  end
-
-  
-  private
-
-  def set_card
-    # @credit_card = current_user.credit_cards.first if current_user.credit_cards.present?
-    # もし存在していたらという記述
-  end
 end
