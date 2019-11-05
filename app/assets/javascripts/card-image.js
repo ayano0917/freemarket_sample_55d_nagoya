b01d774626845e908698051a48f76a5dd76d686f
tomoharu1205 <tomo611205434@gamil.com>
2019-11-05 11:46:36 +0900

    クレジットカード画像表示

--- app/assets/javascripts/card-image.js
+++ app/assets/javascripts/card-image.js
@@ -1,18 +1,20 @@
 $(function() {
-  $('#card_image').on({
-    'mouseenter': function() {
-      let html = `
+  $('#card-image').on({
+    click: function() {
+      let p = $('div').hasClass('main-container__chapter__content__security-code__card-image');
+      if(p) {
+    $('#add').remove();
+      } else {
+        let html = `
     <div class="main-container__chapter__content__security-code__card-image", id="add" >
       カードの裏面をご参照ください。
       <br>
       <img src="//www-mercari-jp.akamaized.net/assets/img/signup/seq-card.png?465897195" alt="" width="240">
     </div>
     `;
-    $('#card_image').append(html);
-    },
-    'mouseleave': function() {
-      $('.main-container__chapter__content__security-code__card-image').remove();
-    }
-  })
+    $('#card-image').append(html);
+      }
+  }
+})
 });
 
