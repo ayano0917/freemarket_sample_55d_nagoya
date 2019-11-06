e0bc4aac189bca8173507bd74e602db4f455d2c0
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-11-06 22:08:37 +0900

    アイテム主その他商品表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,10 +7,10 @@ class ItemsController < ApplicationController
     @item = Item.find(1) #商品出品未実装のため仮idで対応
     @user = User.find_by(id: @item.seller_id)
     @category = Category.find(@item.category_id).name
-    @images = @item.images
+
     # ユーザーの他の商品
     @items = Item.where(seller_id: @user.id).where.not(id: @item.id).limit(6).order("id ASC")
-    @prices = @items.map{|item| item.price} 
+    @images = @items.map{|item| item.images[0]} 
   end
 
   def new
