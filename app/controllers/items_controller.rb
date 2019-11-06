9348931db1ff66395afa6d0ab0212067b260eb2e
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-11-06 21:02:27 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,11 +5,11 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(1) #商品出品未実装のため仮idで対応
-    @user = User.find_by(id: @item.user_id)
+    @user = User.find_by(id: @item.seller_id)
     @category = Category.find(@item.category_id).name
-    @images = @item.images 
+    @images = @item.images
     # ユーザーの他の商品
-    @items = Item.where.not(id: @item.id).limit(6).order("id ASC")
+    @items = Item.where(seller_id: @user.id).where.not(id: @item.id).limit(6).order("id ASC")
     @prices = @items.map{|item| item.price} 
   end
 
