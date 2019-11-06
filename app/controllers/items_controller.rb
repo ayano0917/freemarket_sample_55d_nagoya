a50813b48908e1b8cd92cfab0b1a814ad73e802f
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-11-06 14:19:53 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,13 +4,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(1)
+    @item = Item.find(1) #商品出品未実装のため仮idで対応
     @user = User.find_by(id: @item.user_id)
     @category = Category.find(@item.category_id).name
     @images = @item.images 
     # ユーザーの他の商品
     @items = Item.where.not(id: @item.id).limit(6).order("id ASC")
-    @prices = @items.map{|item| item.price}    
+    @prices = @items.map{|item| item.price} 
   end
 
   def new
@@ -20,27 +20,5 @@ class ItemsController < ApplicationController
   end
 
   def confirm
-  end
-  
-  private
-  def item_params
-    params.require(:item).permit(
-        :user_id,
-        :name, 
-        :description, 
-        :condition_id, 
-        :shipping_fee_id,
-        :shipping_form_id,
-        :prefecture_id, 
-        :days_before_shipping_id,
-        :size_id,
-        :brand,
-        :buyer_id,
-        :seller_id,
-        :category,
-        :price,
-        :shipping_address,
-        :status
-      )
   end
 end
