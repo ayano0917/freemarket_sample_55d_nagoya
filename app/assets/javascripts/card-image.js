f934b4820ecd00666cb7b7009928e25b18abdcee
tomoharu1205 <tomo611205434@gamil.com>
2019-11-09 17:07:49 +0900

    updata

--- app/assets/javascripts/card-image.js
+++ app/assets/javascripts/card-image.js
@@ -1,7 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $('#card-image').on({
     click: function() {
-      $("#add-card").validate(
       let p = $('div').hasClass('main-container__chapter__content__security-code__card-image');
       if(p) {
         $('#add').remove();
