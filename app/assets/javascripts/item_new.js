4bcaba72297c42e52b82a2027710d6c57b98bc31
ayano0917 <yuki1306ni@yahoo.co.jp>
2019-11-10 19:07:17 +0900

    画像表示完了

--- app/assets/javascripts/item_new.js
+++ app/assets/javascripts/item_new.js
@@ -4,6 +4,7 @@ $(function(){
 
 // プレビューBOXの生成
   function buildPreviewHTML(e, image_count) {
+    console.log(1)
     var html =`<li class='upload-item'>
                 <div class='upload-item__square'>
                   <img src='${e.target.result}' class='image-preview' data-image=${image_count}>
@@ -18,7 +19,8 @@ $(function(){
 
   // アップロードBOXの生成
   function buildInputImageBox(image_count){
-    var html = `<div class='upload-container__file-box.have-item-${$('.upload-item').length+1}' data-image=${image_count}>
+    console.log(2)
+    var html = `<div class='upload-container__file-box' id='have-item-${$('.upload-container__items').length+1}' data-image=${image_count}>
                   <input class='upload-container__file${image_count}' type="file">
                   <pre class='upload-container--sentence'>
                     クリックしてファイルをアップロード
@@ -30,12 +32,15 @@ $(function(){
   // 画像のプレビュー表示
   function readURL(input) {
     if (input.files && input.files[0]) {
+      console.log(3)
       var reader = new FileReader();
       reader.onload = function (e) {
         var insertHTML = buildPreviewHTML(e, image_count);
-        if (($('#item-container1').children().length) < 5) {
-          $('#item-container1').append(insertHTML);
+        if (($('#item-preview1').children().length) < 5) {
+          console.log(4)
+          $('#item-preview1').append(insertHTML);
         } else {
+          console.log(5)
           $('#item-container2').removeClass('hide');
           $('#item-container2').append(insertHTML);
         }
@@ -46,12 +51,14 @@ $(function(){
       $(input).parent().addClass('hide');
       $('.upload-item').append(buildInputImageBox(image_count+input.files.length));
       if ($('#item-container2').children().length == 4) {
+        console.log(6)
         $('.upload-container__file-box').last().addClass('hide');
       }
     }
   }
 
   $(document).on('change', '.upload-container__file', function(){
+    console.log(7)
     readURL(this);
   });
 })
