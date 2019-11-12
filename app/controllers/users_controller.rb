be0d91858774f5755aa935447900331c428ca54d
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-12 13:56:27 +0900

    不要なアクションの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,6 @@
 class UsersController < ApplicationController
 
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
-
   def update
-  end
-
-  def edit
-  end
-
-  def identification
-    # @user_address = UserAddress.find_by(user_id: current_user.id)
-  end
-
-  def logout
-  end
-
-  def complete
-  end
-
-  def show_creaditcard
-    #マイページー設定ー支払い方法ー新規作成
-  end
-
-  def new_creaditcard
-    #クレカの登録がない時に、カードの詳細を入力する画面
   end
   
 end
