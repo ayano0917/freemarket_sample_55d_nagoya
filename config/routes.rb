5bb1da15ce7b2c3ffae4d90ec2c42587eaffde33
tomoharu1205 <tomo611205434@gamil.com>
2019-11-14 19:32:28 +0900

    コントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
 
+  get 'purchases/new'
+
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks",
     sessions: 'users/sessions'
