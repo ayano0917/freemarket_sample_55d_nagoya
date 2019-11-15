a8e4de0c54f786bfceb33981af26d1d899e9787a
ayano0917 <yuki1306ni@yahoo.co.jp>
2019-11-16 00:53:59 +0900

    実験中

--- app/assets/javascripts/item_new.js
+++ app/assets/javascripts/item_new.js
@@ -5,7 +5,7 @@ $(function(){
 // プレビューBOXの生成
   function buildPreviewHTML(e, image_count) {
     console.log(1)
-    var html =`<div class='upload-images'>
+    var html =` <div class='upload-images'>
                 <ul>
                   <li class='upload-item'>
                     <div class='upload-item__square'>
@@ -17,29 +17,30 @@ $(function(){
                     </div>
                   </li>
                 </ul>
-              </div>`;
+                </div>`;
     return html;
   }
 
   // アップロードBOXの生成
-  function buildInputImageBox(image_count){
-    console.log(8)
-    var html = `<div class='upload-container__file-box' id='have-item-${$('.upload-container__items').length}' data-image=${image_count}>
-                  <input class='upload-container__file${image_count}' type="hidden">
-                  <pre class='upload-container--sentence'>
-                    クリックしてファイルをアップロード
-                  </pre>
-                </div>`
-                console.log($('.upload-container__items'))
-                console.log($('.upload-container__items').length)
-                console.log(html)
-    return html
-  }
+  // function buildInputImageBox(image_count){
+  //   console.log(8)
+  //   var html = `<div class='upload-container__file-box' id='have-item-${$('.upload-container__items').length}' data-image=${image_count}>
+  //                 <input class='upload-container__file${image_count}' type="hidden">
+  //                 <pre class='upload-container--sentence'>
+  //                   クリックしてファイルをアップロード
+  //                 </pre>
+  //               </div>`
+  //               console.log($('.upload-container__items'))
+  //               console.log($('.upload-container__items').length)
+  //               console.log(html)
+  //   return html
+  // }
 
   // 画像のプレビュー表示
   function readURL(input) {
     if (input.files && input.files[0]) {
-      console.log(3)
+      console.log(88)
+      console.log($(input.files).length)
       var reader = new FileReader();
       reader.onload = function (e) {
         var insertHTML = buildPreviewHTML(e, image_count);
@@ -54,13 +55,13 @@ $(function(){
         console.log(12)
         image_count += 1;
       }
-      console.log(11)
+      // console.log(11)
       reader.readAsDataURL(input.files[0]);
-
       // 親要素取得しhide、appendする→新アップロードBOX作成
       $(input).parent().addClass('hide');
       console.log(15)
-      $('.upload-images').append(buildInputImageBox(image_count+input.files.length));
+      // $('.upload-images').append(buildInputImageBox(image_count+input.files.length));
+
       if ($('#item-container2').children().length == 4) {
         console.log(6)
         $('.upload-container__file-box').last().addClass('hide');
@@ -68,36 +69,41 @@ $(function(){
     }
   }
 
-  // プレビュー表示を読み込む
+
+  // プレビュー表示を読み込む★
   $(document).on('change', '.upload-container__file', function(){
     console.log(7)
     readURL(this);
+    if (image_count == 0 || image_count ==5){
+      console.log(20)
+      $('.upload-container').width(490);
+    }
   });
 
-  // アップロードBOXの削除(have-item-?)と連動
-  $(document).on('click','.upload-item__btn-box--delete', function(){
-    console.log(13)
-    var delete_box = $(this).parent().parent();
-    var uploaded_num = $(this).attr('data-image');
-  // parent先のアップロードBoxの削除
-    delete_box.remove();
-    // 削除画像が元画像か今回追加画像か
-    if (uploaded_num <= saved_image_num) {
-      // 元画像→配列要素0
-      $('saved-image-'+ uploaded_num).val(0);
-    } else {
-      // 追加画像の場合
-      $('upload-container__file-box').each(function(index, element){
-        if ($(element).data('image')==uploaded_num) {
-          console.log(9)
-          $(element).remove();
-        }
-      });
-    }
+  // // アップロードBOXの削除(have-item-?)と連動
+  // $(document).on('click','.upload-item__btn-box--delete', function(){
+  //   console.log(13)
+  //   var delete_box = $(this).parent().parent();
+  //   var uploaded_num = $(this).attr('data-image');
+  // // parent先のアップロードBoxの削除
+  //   delete_box.remove();
+  //   // 削除画像が元画像か今回追加画像か
+  //   if (uploaded_num <= saved_image_num) {
+  //     // 元画像→配列要素0
+  //     $('saved-image-'+ uploaded_num).val(0);
+  //   } else {
+  //     // 追加画像の場合
+  //     $('upload-container__file-box').each(function(index, element){
+  //       if ($(element).data('image')==uploaded_num) {
+  //         console.log(9)
+  //         $(element).remove();
+  //       }
+  //     });
+  //   }
     // アップロードBOXのサイズ変更のため、クラス名の追加&削除
     $('.upload-container__file-box').last().removeClass(function(index, className){
       console.log($('.upload-container__file-box'))
       return (className.match(/\bhave-item-\S+/g) || []).join(' ');
     });
   })
-})
+// })
