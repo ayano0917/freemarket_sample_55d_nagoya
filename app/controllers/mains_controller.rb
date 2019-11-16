9646dd7ce30fc2b5a3056f4cd13b75b54b8fd9cb
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-11-16 11:58:50 +0900

    コントローラ記述

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,6 +1,23 @@
 class MainsController < ApplicationController
-
+  before_action :get_publishing_item, only: [:index]
   def index
+    ranking1 = @publishing_item.group(:parent_id).order('count_parent_id DESC').limit(4).count(:parent_id).keys
+    
+    @category1 = Category.find_by(id: ranking1[0])
+    @items1 = @publishing_item.where(parent_id: ranking1[0]).order('id DESC').limit(4)
+    
+    @category2 = Category.find_by(id: ranking1[1])
+    @item2 = @publishing_item.where(parent_id: ranking1[1]).order('id DESC').limit(4)
+    
+    @category3 = Category.find_by(id: ranking1[2])
+    @item3 = @publishing_item.where(parent_id: ranking1[2]).order('id DESC').limit(4)
+
+    @category4 = Category.find_by(id: ranking1[3])
+    @item4 = @publishing_item.where(parent_id: ranking1[3]).order('id DESC').limit(4)
+  end
+
+  def get_publishing_item
+    @publishing_item = Item.where(buyer_id: nil)
   end
 
 end
