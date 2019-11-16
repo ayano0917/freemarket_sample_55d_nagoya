3108869a357bff9aa9f5058d19410f066a74a3fa
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-16 14:12:03 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(current_user)
-    if @user.update(params[:user][:profile])
-      redirect_to root
+    if @user.update_attributes(profile: params[:user][:profile], nickname: params[:user][:nickname])
+      redirect_to profile_user_mypage_path(current_user)
     else
-      render root(current_user)
+      render profile_user_mypage_path(current_user)
     end
   end
 
