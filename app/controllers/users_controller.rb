de700e7853aefe79630cb71eaf0f588fe773bc1f
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-16 15:09:19 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
   def update
     @user = User.find(current_user)
     if @user.update_attributes(profile: params[:user][:profile], nickname: params[:user][:nickname])
-      redirect_to profile_user_mypage_path(current_user)
+      redirect_to profile_user_mypage_path(current_user), notice: '変更しました。'
     else
-      render profile_user_mypage_path(current_user)
+      render profile_user_mypage_path(current_user), alert: '変更できませんでした。'
     end
   end
 
