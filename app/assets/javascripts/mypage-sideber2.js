1de938151271d433d807b4cd1e76aa0e2da30a18
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-17 14:57:35 +0900

    turbolinks:loadを追加

--- app/assets/javascripts/mypage-sideber2.js
+++ app/assets/javascripts/mypage-sideber2.js
@@ -1,23 +1,25 @@
-$(function() {
-  let focus1 = $('.wrapper__sidebar__nav__list__1__link');
-  let focus2 = $('.wrapper__sidebar__nav__list2__1__link');
-  let focus3 = $('.wrapper__sidebar__nav__list3__1__link');
-  let icon1 = $('.wrapper__sidebar__nav__list__1__link__icon');
+$(document).on('turbolinks:load',function() {
+  $(function() {
+    let focus1 = $('.wrapper__sidebar__nav__list__1__link');
+    let focus2 = $('.wrapper__sidebar__nav__list2__1__link');
+    let focus3 = $('.wrapper__sidebar__nav__list3__1__link');
+    let icon1 = $('.wrapper__sidebar__nav__list__1__link__icon');
 
-  focus1.add(focus2).add(focus3).hover(function() {
-    $(this).css('background-color', '#FAFAFA');
-    $(this).children(icon1).css("color", "#333");
-    $(this).children(icon1).animate({
-      top: "11px",
-      right: "12px",
-    }, 200);
-  },(function() {
-    $(this).css('background-color', '#FFFFFF');
-    $(this).children(icon1).css("color", "#ccc");
-    $(this).children(icon1).animate({
-      top: "11px",
-      right: "16px",
-    }, 200);
-  })
-  )
-});
+    focus1.add(focus2).add(focus3).hover(function() {
+      $(this).css('background-color', '#FAFAFA');
+      $(this).children(icon1).css("color", "#333");
+      $(this).children(icon1).animate({
+        top: "11px",
+        right: "12px",
+      }, 200);
+    },(function() {
+      $(this).css('background-color', '#FFFFFF');
+      $(this).children(icon1).css("color", "#ccc");
+      $(this).children(icon1).animate({
+        top: "11px",
+        right: "16px",
+      }, 200);
+    })
+    )
+  });
+})
