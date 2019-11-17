0d49d6c72cebd4e39af168b9a632b603b7d87562
tomoharu1205 <tomo611205434@gamil.com>
2019-11-17 21:16:21 +0900

    updata

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path, notice: '商品を出品しました。'
+      redirect_to done_items_path
     else
       redirect_to new_item_path
     end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
   # アイテム用のイメージモデル
-  belongs_to :item
+  belongs_to :item, optional: true
   mount_uploader :image, ImageUploader
 end
