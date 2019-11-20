2ea0785687e0983e3f985f3b9ab9c4669fd00a5b
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-11-20 17:17:03 +0900

    テーブル記述

--- db/migrate/20191120080917_create_category_sizes.rb
+++ db/migrate/20191120080917_create_category_sizes.rb
@@ -1,6 +1,8 @@
 class CreateCategorySizes < ActiveRecord::Migration[5.0]
   def change
     create_table :category_sizes do |t|
+      t.integer :category_id, null: false, index: true, foreign_key: true
+      t.integer :size_id, null: false, index: true, foreign_key: true
 
       t.timestamps
     end
