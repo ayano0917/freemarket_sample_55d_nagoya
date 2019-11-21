076b18c6ee92e961385bfea7a2c5de58d1ac623d
tomoharu1205 <tomo611205434@gamil.com>
2019-11-21 10:37:49 +0900

    updata

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,4 +1,4 @@
-item# Be sure to restart your server when you modify this file.
+# Be sure to restart your server when you modify this file.
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
