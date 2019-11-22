5b75e9b603a88e4f644310ef1bd3c7c504c46245
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-11-22 13:13:39 +0900

    アソシエーション追記・編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_form
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :days_before_shipping
-  belongs_to_active_hash :size
   # belongs_to :brand
+  belongs_to :size, optional: true
   belongs_to :category, optional: true
   belongs_to :seller, class_name: "User", foreign_key: 'seller_id'
   belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id', optional: true
