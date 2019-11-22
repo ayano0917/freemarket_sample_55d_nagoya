23080222d6ecf9a4f237951ee9fa227fd56a37ff
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-11-22 22:38:43 +0900

    save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,19 @@
 class CommentsController < ApplicationController
 
-  def new
+  def create
+    @comment = Comment.new(comment_params)
+    if user_sign_in?
+      @comment.save
+      redirect_to item_path
+    end
+
+
   end
 
-  def create
+  private
+
+  def comment_params
+    params.permit(:content, :item_id)
   end
   
 end
