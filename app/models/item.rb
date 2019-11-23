a0d65bb797eb470987c35ef3d9126996124fd58d
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-11-23 19:00:51 +0900

    Revert "save"
    This reverts commit 14aa5b32c5581ff8ceb00e28c0a7b4735439318b.

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: "User", foreign_key: 'seller_id'
   belongs_to :buyer, class_name: "User", foreign_key: 'buyer_id', optional: true
   # belongs_to :shipping_address
-  has_many :comments, dependent: :destroy
+  #has_many :comments, dependent: :destroy
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   accepts_nested_attributes_for :brand
