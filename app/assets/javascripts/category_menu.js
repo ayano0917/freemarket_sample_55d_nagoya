70232e1430a0c34cdd29f79cc39b75308a87ff19
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-30 20:12:47 +0900

    hoverにより色の変更

--- app/assets/javascripts/category_menu.js
+++ app/assets/javascripts/category_menu.js
@@ -3,76 +3,59 @@ $(document).on('turbolinks:load', function() {
     let category = $('.main-header__inner__nav__search__category__link');
     let category_list = $('.main-header__inner__nav__search__category__link__inner__lists');
 
-    let brand = $('.main-header__inner__nav__search__brand__link')
+    let header_brand = $('.main-header__inner__nav__search__brand__link');
+    let header_like = $('.main-header__inner__nav__menu__like__link');
+    let header_notification = $('.main-header__inner__nav__menu__notification__link');
+    let header_todo = $('.main-header__inner__nav__menu__todo__link');
+    let header_mypage = $('.main-header__inner__nav__menu__mypage__link');
 
     category.hover(function() {
       $('.header1').css("color", "#0295EE");
       category_list.show();
-      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+      category_list.append('<li class="header__category__list">レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
     },(function() {
       $('.header1').css("color", "#333333");
       category_list.hide();
-      $('li').remove();
+      $('header__category__list').remove();
       })
     );
 
-    brand.hover(function() {
+    header_brand.hover(function() {
       $('.header2').css("color", "#0295EE");
-      category_list.show();
-      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
     },(function() {
       $('.header2').css("color", "#333333");
-      category_list.hide();
-      $('li').remove();
       })
     );
 
-    // 下記ログイン時
-
-    brand.hover(function() {
-      $('.header2').css("color", "#0295EE");
-      category_list.show();
-      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+    header_like.hover(function() {
+      $('.header3, .main-header__inner__nav__menu__like__link__icon').css("color", "#0295EE");
     },(function() {
-      $('.header2').css("color", "#333333");
-      category_list.hide();
-      $('li').remove();
+      $('.main-header__inner__nav__menu__like__link__icon').css("color", "#cccccc")
+      $('.header3').css("color", "#3d3d3d");
       })
     );
 
-    brand.hover(function() {
-      $('.header2').css("color", "#0295EE");
-      category_list.show();
-      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+    header_notification.hover(function() {
+      $('.header4, .main-header__inner__nav__menu__notification__link__icon').css("color", "#0295EE");
     },(function() {
-      $('.header2').css("color", "#333333");
-      category_list.hide();
-      $('li').remove();
+      $('.main-header__inner__nav__menu__notification__link__icon').css("color", "#cccccc")
+      $('.header4').css("color", "#3d3d3d");
       })
     );
 
-    brand.hover(function() {
-      $('.header2').css("color", "#0295EE");
-      category_list.show();
-      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+    header_todo.hover(function() {
+      $('.header5, .main-header__inner__nav__menu__todo__link__icon').css("color", "#0295EE");
     },(function() {
-      $('.header2').css("color", "#333333");
-      category_list.hide();
-      $('li').remove();
+      $('.main-header__inner__nav__menu__todo__link__icon').css("color", "#cccccc")
+      $('.header5').css("color", "#3d3d3d");
       })
     );
 
-    brand.hover(function() {
-      $('.header2').css("color", "#0295EE");
-      category_list.show();
-      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+    header_mypage.hover(function() {
+      $('.header6').css("color", "#0295EE");
     },(function() {
-      $('.header2').css("color", "#333333");
-      category_list.hide();
-      $('li').remove();
+      $('.header6').css("color", "#3d3d3d");
       })
     );
-
-
   });
 });
