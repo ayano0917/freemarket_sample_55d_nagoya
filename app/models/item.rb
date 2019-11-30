2be987e93b858b09c45c9f198f5f16db3b139178
ayano0917 <yuki1306ni@yahoo.co.jp>
2019-11-30 17:39:32 +0900

    バージョン２導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,40 +15,56 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :images, allow_destroy: true
   accepts_nested_attributes_for :brand
   
-  validates :name,
-  presence: {message: '商品名を入力してください'},
+  validates :name,presence: true,
   length: { maximum: 40, message: '40文字以下で入力してください'}
-
   validates :description,
   presence: {message: '商品の説明を入力してください'},
   length: { maximum: 1000, message: '1000文字以下で入力してください'}
-
   validates :parent_id,
   presence: {message: 'カテゴリを選択してください'},
   numericality: { only_integer: true}
-
   validates :child_id,
   presence: {message: 'カテゴリを選択してください'},
   numericality: { only_integer: true}
-
   validates :condition_id,
   presence: {message: '商品の状態を選択してください'}
-
   validates :shipping_fee_id,
   presence: {message: '配送料の負担について選択してください'}
-
   validates :shipping_form_id,
   presence: {message: '配送料の方法について選択してください'}
-
   validates :prefecture_id,
   presence: {message: '発送元の地域を選択してください'}
-
   validates :days_before_shipping_id,
   presence: {message: '発送までの日数を選択してください'}
-
   validates :price,
   presence: {message: '販売価格を入力してください'},
   numericality: { only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: '数値かつ300円以上にて入力してください'}
+
+  # validates :name,
+  # presence: {message: '商品名を入力してください'},
+  # length: { maximum: 40, message: '40文字以下で入力してください'}
+  # validates :description,
+  # presence: {message: '商品の説明を入力してください'},
+  # length: { maximum: 1000, message: '1000文字以下で入力してください'}
+  # validates :parent_id,
+  # presence: {message: 'カテゴリを選択してください'},
+  # numericality: { only_integer: true}
+  # validates :child_id,
+  # presence: {message: 'カテゴリを選択してください'},
+  # numericality: { only_integer: true}
+  # validates :condition_id,
+  # presence: {message: '商品の状態を選択してください'}
+  # validates :shipping_fee_id,
+  # presence: {message: '配送料の負担について選択してください'}
+  # validates :shipping_form_id,
+  # presence: {message: '配送料の方法について選択してください'}
+  # validates :prefecture_id,
+  # presence: {message: '発送元の地域を選択してください'}
+  # validates :days_before_shipping_id,
+  # presence: {message: '発送までの日数を選択してください'}
+  # validates :price,
+  # presence: {message: '販売価格を入力してください'},
+  # numericality: { only_integer: true , greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: '数値かつ300円以上にて入力してください'}
 
   def previous
     Item.where("id < ?",id).order("id DESC").first
