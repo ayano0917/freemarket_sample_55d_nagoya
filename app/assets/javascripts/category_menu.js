bc10fa57ec3fcb45e97a508209c9d876c817d9e8
KoseiMuramatsu <mukamura219@gmail.com>
2019-11-30 19:20:16 +0900

    category_menu.jsファイル編集

--- app/assets/javascripts/category_menu.js
+++ app/assets/javascripts/category_menu.js
@@ -27,5 +27,52 @@ $(document).on('turbolinks:load', function() {
       })
     );
 
+    // 下記ログイン時
+
+    brand.hover(function() {
+      $('.header2').css("color", "#0295EE");
+      category_list.show();
+      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+    },(function() {
+      $('.header2').css("color", "#333333");
+      category_list.hide();
+      $('li').remove();
+      })
+    );
+
+    brand.hover(function() {
+      $('.header2').css("color", "#0295EE");
+      category_list.show();
+      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+    },(function() {
+      $('.header2').css("color", "#333333");
+      category_list.hide();
+      $('li').remove();
+      })
+    );
+
+    brand.hover(function() {
+      $('.header2').css("color", "#0295EE");
+      category_list.show();
+      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+    },(function() {
+      $('.header2').css("color", "#333333");
+      category_list.hide();
+      $('li').remove();
+      })
+    );
+
+    brand.hover(function() {
+      $('.header2').css("color", "#0295EE");
+      category_list.show();
+      category_list.append('<li>レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+    },(function() {
+      $('.header2').css("color", "#333333");
+      category_list.hide();
+      $('li').remove();
+      })
+    );
+
+
   });
 });
