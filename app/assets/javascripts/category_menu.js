649dc4fe8b44331166c8eb32b7bc358023f54f80
KoseiMuramatsu <mukamura219@gmail.com>
2019-12-01 09:19:12 +0900

    カテゴリーから探す・ブランドから探すhtml要素表示機能追加

--- app/assets/javascripts/category_menu.js
+++ app/assets/javascripts/category_menu.js
@@ -1,29 +1,36 @@
 $(document).on('turbolinks:load', function() {
   $(function() {
     let category = $('.main-header__inner__nav__search__category__link');
-    let category_list = $('.main-header__inner__nav__search__category__link__inner__lists');
+    let header_category = $('.category__header');
+    let header_category_lists = $('.category__header__list');
 
     let header_brand = $('.main-header__inner__nav__search__brand__link');
+    let header_brand_inner = $('.brand__header')
+    let header_brand_lists = $('.brand__header__list')
+
     let header_like = $('.main-header__inner__nav__menu__like__link');
+
     let header_notification = $('.main-header__inner__nav__menu__notification__link');
+
     let header_todo = $('.main-header__inner__nav__menu__todo__link');
+
     let header_mypage = $('.main-header__inner__nav__menu__mypage__link');
 
     category.hover(function() {
       $('.header1').css("color", "#0295EE");
-      category_list.show();
-      category_list.append('<li class="header__category__list">レディース<br>メンズ<br>ベビー・キッズ<br>インテリア・住まい・小物<br>本・音楽・ゲーム<br>おもちゃ・ホビー・グッズ<br>コスメ・香水・美容<br>スポーツ・レジャー<br>ハンドメイド<br>チケット<br>自動車・オートバイ<br>その他<br>カテゴリー一覧</li>');
+      header_category.add(header_category_lists).removeAttr('hidden');
     },(function() {
       $('.header1').css("color", "#333333");
-      category_list.hide();
-      $('header__category__list').remove();
+      header_category.add(header_category_lists).attr('hidden', '');
       })
     );
 
     header_brand.hover(function() {
       $('.header2').css("color", "#0295EE");
+      header_brand_inner.add(header_brand_lists).removeAttr('hidden');
     },(function() {
       $('.header2').css("color", "#333333");
+      header_brand_inner.add(header_brand_lists).attr('hidden', '');
       })
     );
 
