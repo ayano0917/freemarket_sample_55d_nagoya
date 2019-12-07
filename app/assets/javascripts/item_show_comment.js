ff4bce52a8ee9e10a905ff8fecf146700eb36777
hsbsts1988 <hasebesatoshi1107@gmail.com>
2019-12-07 11:59:51 +0900

    push

--- app/assets/javascripts/item_show_comment.js
+++ app/assets/javascripts/item_show_comment.js
@@ -2,8 +2,8 @@ $(function() {
   $("#t-area").keyup(function() {
     var text = $(this).val();
   if (text.length >= 1) {
-  $("#s-btn").css("background-color","red");
-  $("#s-btn").css("border-color", "red");
+  $("#s-btn").css("background-color","#EA342D");
+  $("#s-btn").css("border-color", "#EA342D");
   }
   else {
   $("#s-btn").css("background-color","");
